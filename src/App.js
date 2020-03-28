04e9ced4fc1e8911cfe24c8f6e87081e265fdbf9
Oskar <oskarino.g@gmail.com>
2020-03-28 12:54:01 +0000

    Added files from Benji

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React from "react";
-import AppNavbar from "./components/AppNavbar";
-import CarList from "./components/CarList";
+import React from 'react';
+import AppNavbar from './components/AppNavbar';
+import AppCarousel from './components/AppCarousel';
+import Listings from './components/Listings';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ClassicalCars from './components/pages/ClassicCars';
 
-// Redux imports
-import { Provider } from "react-redux";
-import store from "./store";
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <AppNavbar />
-        <CarList />
+    <Router>
+      <div className="Nav">
+        <Route exact path="/" render={props => (
+          <React.Fragment>
+            <AppNavbar />
+            <AppCarousel />
+          </React.Fragment>
+        )}/>
+        <Route path="/classical-cars" component={ClassicalCars} />
+          <Listings />
       </div>
-    </Provider>
+    </Router>
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
