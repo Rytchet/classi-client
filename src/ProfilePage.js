5cd60c134d4062f9630f69b1e2d2c5650343e410
Oskar <oskarino.g@gmail.com>
2020-04-08 15:21:39 +0100

    Added a basic profile page

---
+++ src/ProfilePage.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import AppNavbar from './components/AppNavbar';
+import axios from 'axios';
+
+export default class ProfilePage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: {},
+      isLoaded: false,
+    };
+  }
+
+  componentDidMount() {
+    const localUser = JSON.parse(localStorage.getItem('user'));
+    axios
+      .get('/users/' + localUser.id)
+      .then(res => {
+        console.log(res);
+        this.setState({
+          isLoaded: true,
+          user: res.data,
+          localUser,
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    if (!this.state.isLoaded) {
+      return <div>Page is loading</div>;
+    }
+
+    const { localUser, user } = this.state;
+
+    return (
+      <div>
+        <AppNavbar user={localUser} />
+        <img
+          src={'https://classi-server.herokuapp.com' + user.avatar_url}
+          height="400"
+          width="400"
+          alt="avatar"
+        ></img>
+        <p>Name: {user.name}</p>
+        {/* Get other details the same way */}
+      </div>
+    );
+  }
+}
