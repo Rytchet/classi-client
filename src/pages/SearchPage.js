60d99995a80bb7e676c0f5f7d538933904d23815
Oskar <oskarino.g@gmail.com>
2020-04-13 14:16:59 +0100

    Add filters

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -3,6 +3,8 @@ import AppNavbar from '../components/AppNavbar';
 import Listings from '../components/Listings';
 import queryString from 'query-string';
 
+import { Form, Container, Button } from 'react-bootstrap';
+
 import axios from 'axios';
 
 export class SearchPage extends Component {
@@ -12,7 +14,33 @@ export class SearchPage extends Component {
     this.state = {
       user: {},
       listings: [{ id: 'xd' }],
+      make: '',
+      model: '',
+      year: '',
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let { make, model, year } = this.state;
+    axios
+      .get('/listings/search', {
+        params: { 'car.make': make, 'car.model': model, 'car.year': year },
+      })
+      .then(res => {
+        this.setState(prevState => ({
+          listings: [...res.data],
+          user: JSON.parse(localStorage.getItem('user')),
+        }));
+      });
   }
 
   componentDidMount() {
@@ -26,11 +54,46 @@ export class SearchPage extends Component {
   }
 
   render() {
-    let { user, listings } = this.state;
+    let { user, listings, make, model, year } = this.state;
 
     return (
       <div>
         <AppNavbar user={user} />
+        <Container>
+          <Form onSubmit={this.handleSubmit} className="form-inline">
+            <Form.Label className="m-2">Make</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Make"
+              name="make"
+              value={make}
+              onChange={this.handleChange}
+            ></Form.Control>
+
+            <Form.Label className="m-2">Model</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Model"
+              name="model"
+              value={model}
+              onChange={this.handleChange}
+            ></Form.Control>
+
+            <Form.Label className="m-2">Year</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Year"
+              name="year"
+              value={year}
+              onChange={this.handleChange}
+            ></Form.Control>
+
+            <Button type="submit" className="m-2">
+              Filter
+            </Button>
+          </Form>
+        </Container>
+
         {listings.length > 0 && <Listings listings={listings} />}
         {listings.length === 0 && <h1>No listings found</h1>}
       </div>
