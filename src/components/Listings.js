c35df068f22a8f3a9d0c730846147191de957e19
BenjaminHardwick <benhefc14@gmail.com>
2020-04-17 19:35:24 +0100

    reverted last commit

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Card, CardColumns, Spinner } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import 'postboot/dist/css/postboot.min.css';
 
 class Listings extends Component {
   constructor(props) {
@@ -34,40 +33,49 @@ class Listings extends Component {
     }
 
     return (
-      <Container>
-        <h1>Featured Listings</h1>
-
+      <Container className="mt-5">
         {this.state.listings.map(listing => (
           <Link to={'/listing/' + listing._id} key={listing._id}>
-            <div class="card flex-sm-row mb-gutter">
-              <img
-                style={{ width: 'auto', height: '20vh' }}
-                className="card-img-sm-left"
-                src={listing.photos[0]}
-              />
-              <div class="card-body">
-                <h4 class="card-title">{listing.title}</h4>
-                <p class="card-text">
-                  {listing.car.year} {listing.car.make} {listing.car.model}
-                </p>
+            <div class="container py-2">
+              <div class="card flex-row">
+                <img
+                  style={{
+                    overflow: 'hidden',
+                    width: '25%',
+                    height: '100%',
+                    backgroundImage: `url(${listing.photos[0]})`,
+                    display: 'block',
+                    backgroundPosition: '50% 50%',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover',
+                  }}
+                  class="fluid-image rounded-left"
+                  src={listing.photos[0]}
+                  alt="Card image cap"
+                />
+                <div class="d-flex flex-column flex-grow-1">
+                  <div class="card-body w-40">
+                    <h5 class="card-title">{listing.title}</h5>
+                    <h3 class="card-text">
+                      {listing.car.year} {listing.car.make} {listing.car.model}
+                    </h3>
+                    <hr />
+                    <b class=".bg-light-gray">
+                      £
+                      {listing.price.toLocaleString(navigator.language, {
+                        minimumFractionDigits: 2,
+                      })}
+                      <p className="float-right">
+                        <img height="20px" src="/eye-outline.svg"></img> Times
+                        Viewed: {listing.times_viewed}
+                      </p>
+                    </b>
+                  </div>
+                </div>
               </div>
-              <hr />
-              <b className="card-footer">
-                <p>
-                  £
-                  {listing.price.toLocaleString(navigator.language, {
-                    minimumFractionDigits: 2,
-                  })}
-                </p>
-                <p className="float-right">
-                  <img height="20px" src="/eye-outline.svg"></img>{' '}
-                  {listing.times_viewed}
-                </p>
-              </b>
             </div>
           </Link>
         ))}
-
         {/* nMight want to save these and put them into a different file for favourites.
         
         <CardColumns>
